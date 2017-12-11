72bf445f85aed41b8b34bb93ef1e46224db64a39
thomasdenecker <thomas.denecker@gmail.com>
2017-12-11 14:22:51 +0100

    Remove py.test (for the moment)

--- MESH_link/mesh_link.py
+++ MESH_link/mesh_link.py
@@ -7,6 +7,7 @@ December 2017
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
 # -----------------------------------------------------------------------------
 # Import
 # -----------------------------------------------------------------------------
@@ -18,58 +19,65 @@ import argparse
 # Parser
 # -----------------------------------------------------------------------------
 
-PARSER = argparse.ArgumentParser()
-PARSER.add_argument("-m", "--MESH", dest="mesh",
-                    help="Mesh file (bin format find here \
-                    (dYEAR.bin, ie. d2018.bin): \
-                    ftp://nlmpubs.nlm.nih.gov/online/mesh/MESH_FILES/\
-                    asciimesh/)")
-PARSER.add_argument("-o", "--output", dest="output",
-                    help="File name of link table (whithout format (.txt))")
-ARGS = PARSER.parse_args()
 
-if len(sys.argv) != 5:
-    print(len(sys.argv))
-    print(sys.argv)
-    PARSER.error("2 arguments are expected.\
-                 Please, read help to have arguments (sys.argv[0] -h or \
-                 sys.argv[0] --help)")
+def parse_args():
+    """
+    Function to get and parse arguments
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--MESH", dest="mesh",
+                        help="Mesh file (bin format find here \
+                        (dYEAR.bin, ie. d2018.bin): \
+                        ftp://nlmpubs.nlm.nih.gov/online/mesh/MESH_FILES/\
+                        asciimesh/)")
+    parser.add_argument("-o", "--output", dest="output",
+                        help="File name of link table \
+                        (whithout format (.txt))")
+    if len(sys.argv) != 5:
+        parser.error("2 arguments are expected.\
+                     Please, read help to have arguments (sys.argv[0] -h or \
+                     sys.argv[0] --help)")
 
-# -----------------------------------------------------------------------------
-# Variables
-# -----------------------------------------------------------------------------
+    return parser.parse_args()
 
-# MESH is file in ASCII format with Mesh
-# OUTPUT is the name of futur link table
 
-MESH = ARGS.mesh
-OUTPUT = ARGS.output
+if __name__ == '__main__':
 
-# -----------------------------------------------------------------------------
-# Main
-# -----------------------------------------------------------------------------
+    # -------------------------------------------------------------------------
+    # CONSTANT
+    # -------------------------------------------------------------------------
 
-# Read MESH file
-INFILE = open(MESH, "r")
-LINES_MESH = INFILE.readlines()
+    # MESH is file in ASCII format with Mesh
+    # OUTPUT is the name of futur link table
+    ARGS = parse_args()
+    MESH = ARGS.mesh
+    OUTPUT = ARGS.output
 
-# Output file
-OUTFILE = open(OUTPUT+".txt", "w")
-OUTFILE.write("source" + "\t" + "target" + "\n")
+    # -------------------------------------------------------------------------
+    # Main
+    # -------------------------------------------------------------------------
 
-# Search link between MESH term
-for line in LINES_MESH:
-    # MH is a MESH term
-    if line[:5] == "MH = ":
-        MESH_term = line.rstrip()[5:]
+    # Read MESH file
+    INFILE = open(MESH, "r")
+    LINES_MESH = INFILE.readlines()
 
-    # FX is a "see also" for this MESH term
-    if line[:5] == "FX = ":
-        newline = []
-        newline.append(MESH_term)
-        newline.append(line.rstrip()[5:])
-        OUTFILE.write("\t".join(newline)+"\n")
+    # Output file
+    OUTFILE = open(OUTPUT+".txt", "w")
+    OUTFILE.write("source" + "\t" + "target" + "\n")
 
-# Close files
-INFILE.close()
-OUTFILE.close()
+    # Search link between MESH term
+    for line in LINES_MESH:
+        # MH is a MESH term
+        if line[:5] == "MH = ":
+            MESH_term = line.rstrip()[5:]
+
+        # FX is a "see also" for this MESH term
+        if line[:5] == "FX = ":
+            newline = []
+            newline.append(MESH_term)
+            newline.append(line.rstrip()[5:])
+            OUTFILE.write("\t".join(newline)+"\n")
+
+    # Close files
+    INFILE.close()
+    OUTFILE.close()
